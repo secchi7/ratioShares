fa14b7911ca922652e95be5b9267ff98ea08c4ca
Andy <aasecchi@gmail.com>
2024-07-11 18:42:57 -0300

    Se quito el título del gráfico. Se agrego el icono que hace refencia que la solicitud está en progreso.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,6 +9,7 @@ import tickers from "../assets/tickers";
 function Main() {
   const { register, handleSubmit } = useForm();
   const { getResults, results, state } = useResults();
+  const [display, setDisplay] = React.useState(false);
 
   const onSubmit = handleSubmit(async (values) => {
     getResults(values);
@@ -57,7 +58,10 @@ function Main() {
                 </select>
               </div>
             </div>
-            <div className="flex justify-center">
+            <div
+              onClick={() => setDisplay(true)}
+              className="flex justify-center"
+            >
               <button
                 className="bg-[#001b5e] text-gray-100 mt-4 max-w-[100px] w-full p-4 rounded-lg"
                 type="submit"
@@ -66,13 +70,13 @@ function Main() {
               </button>
             </div>
           </form>
-          <div className="content">
+          <div className="content mt-16">
             {state ? (
               <>
                 <div>
-                  <h1 className="sn:text-4xl text-4xl text-center font-bold text-gray-800">
+                  {/* <h1 className="sn:text-4xl text-4xl text-center font-bold text-gray-800">
                     Gráfico
-                  </h1>
+                  </h1> */}
                   <div>
                     <Plot
                       data={[
@@ -85,9 +89,37 @@ function Main() {
                   </div>
                 </div>
               </>
+            ) : display ? (
+              <>
+              <div>
+                  <div class="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md">
+                    <svg
+                      class="animate-spin h-20 w-20 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    {/* Processing... */}
+                  </div>
+                </div>
+              </>
             ) : (
               <>
-                <div></div>
+              
               </>
             )}
           </div>
