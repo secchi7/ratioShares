546c8d023ad0fe8c5b2efd8da315343c6487bddc
Andy <aasecchi@gmail.com>
2024-07-12 12:51:17 -0300

    Cambio ruta api

--- src/api/sendDatas.js
+++ src/api/sendDatas.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const API = "https://ratiosharesbackend.onrender.com/api";
-const API = "http://127.0.0.1:5000/api";
+const API = "https://ratiosharesbackend.onrender.com/api";
+// const API = "http://127.0.0.1:5000/api";
 
 export const sendDataRequest = (share) => axios.post(`${API}/ratio`, share);
